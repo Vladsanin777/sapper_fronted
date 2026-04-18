c152bd1cd1f2af3ef151bc471ff542b9a1ccb88d
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2026-04-18 21:38:18 +0400

    start frontend

---
+++ app/src/main/java/com/example/sapper/MinsField.java
@@ -0,0 +1,41 @@
+package com.example.sapper;
+
+public class MinsField {
+    private long m_nativePtr = 0;
+
+    public MinsField(long rows, long cols, long mins) {
+        init(rows, cols, mins);
+    }
+
+    public void finalize() {
+        destroy();
+    }
+
+    private native void init(long rows, long cols, long mins);
+
+    public native byte getCountMins(long row, long col);
+
+    public native boolean isMin(long row, long col);
+
+    public native void upFlag(long row , long col);
+
+    public native void downFlag(long row , long col);
+
+    public native boolean isFlag(long row, long col);
+
+    public native long getCountRows();
+
+    public native long getCountCols();
+
+    public native boolean isLive();
+
+    public native long getCountFree();
+
+    public native boolean isVictory();
+
+    public native boolean open(long row, long col);
+
+    public native boolean isOpen(long row, long col);
+
+    private native void destroy();
+}

f3ea65af50e4442d51272c8ccdca2b54015af4bb
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2026-04-19 17:58:36 +0400

    intermediate

--- app/src/main/java/com/example/sapper/Game.java
+++ app/src/main/java/com/example/sapper/Game.java
@@ -1,6 +1,37 @@
 package com.example.sapper;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
 
-public class Game extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Game extends AppCompatActivity {
+    MinsField m_field;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.mins_field);
+
+        Intent date = getIntent();
+
+        int count_rows = date.getIntExtra("count_rows", 10);
+        int count_columns = date.getIntExtra("count_columns", 10);
+        int count_mins = date.getIntExtra("count_mins",10);
+        int show_seconds = date.getIntExtra("count_show_seconds", 10);
+
+        m_field = new MinsField(count_rows, count_columns, count_mins);
+
+        TextView rows = findViewById(R.id.count_rows);
+        rows.setText(getString(R.string.count_rows) + count_rows);
+
+        TextView columns = findViewById(R.id.count_columns);
+        columns.setText(getString(R.string.count_columns) + count_columns);
+
+        TextView mins = findViewById(R.id.count_mins);
+        mins.setText(getString(R.string.count_mins) + count_mins);
+    }
 }
--- app/src/main/java/com/example/sapper/MainActivity.java
+++ app/src/main/java/com/example/sapper/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.sapper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+
+import com.shawnlin.numberpicker.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +16,35 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("sapper");
     }
 
+    NumberPicker m_rows;
+    NumberPicker m_columns;
+    NumberPicker m_mins;
+    NumberPicker m_show_seconds;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        m_rows = findViewById(R.id.count_rows);
+
+        m_columns = findViewById(R.id.count_columns);
+        m_mins = findViewById(R.id.count_mins);
+        m_show_seconds = findViewById(R.id.show_seconds);
+
+        Button startGame = findViewById(R.id.start_game_button);
+
+        startGame.setOnClickListener(this::onClickStartGame);
+    }
+
+    public void onClickStartGame(View view) {
+        Intent intent = new Intent(this, Game.class);
+        intent.putExtra("count_rows", m_rows.getValue());
+        intent.putExtra("count_columns", m_columns.getValue());
+        intent.putExtra("count_mins", m_mins.getValue());
+        intent.putExtra("count_show_seconds", m_show_seconds.getValue());
+
+        startActivity(intent);
     }
 }
